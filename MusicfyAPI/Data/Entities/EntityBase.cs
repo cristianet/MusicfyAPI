bedd1fabee40f23ed2924429d97792b7f219e01b
Cristian Carrasco <crxcarrasco@gmail.com>
2022-11-03 19:07:32 -0300

    DDEVS-003 : add soft delete and max albums

---
+++ MusicfyAPI/Data/Entities/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicfyAPI.Data.Entities
+{
+    public abstract class EntityBase : IEntityBase
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public bool IsDeleted { get; set; }
+    }
+
+    public interface IEntityBase
+    {        
+        bool IsDeleted { get; set; }
+    }
+}
